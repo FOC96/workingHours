a4501df2cebe9f3f5aaae442f6d53a3d8e6706b2
Fernando Ortiz Rico Celio <fernando_oc96@hotmail.ocm>
2018-10-20 21:22:08 -0500

    Initial Commit

---
+++ workHours/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workHours
+//
+//  Created by Fernando Ortiz Rico Celio on 10/20/18.
+//  Copyright © 2018 Fernando Ortiz Rico Celio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
